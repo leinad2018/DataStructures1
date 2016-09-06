d3f80ca7c68d205459777d14ceb3cb34a88bcb52
leinad2018 <leinad2018@gmail.com>
2016-09-06 14:03:17 -0500

    Updated fileWriter

--- src/fileWriting/FileWriter.java
+++ src/fileWriting/FileWriter.java
@@ -12,8 +12,14 @@ public class FileWriter {
 	 * @param args
 	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) throws FileNotFoundException {
-		File theFile = new File("H:/HelloWorld.txt");
+	public static void main(String[] args){
+		File theFile;
+		try{
+			theFile = new File("H:/HelloWorld.txt");
+		}catch(Exception e){
+			System.out.println("File could not be found");
+			theFile = new File();
+		}
 		Scanner input = new Scanner(theFile);
 		while(input.hasNextLine()){
 			System.out.println(input.nextLine());
