d6fd753ec33842ba353a3ac0ab69d63fb1de7fce
leinad2018 <leinad2018@gmail.com>
2016-09-23 11:55:47 -0500

    sarer

---
+++ src/queue/CircLinkedUnbndQueue.java
@@ -0,0 +1,41 @@
+package queue;
+
+import support.LLNode;
+
+public class CircLinkedUnbndQueue<T> implements UnboundedQueueInterface<T> {
+
+	protected LLNode<T> rear;
+
+	public CircLinkedUnbndQueue() {
+
+	}
+
+	public T dequeue() throws QueueUnderflowException {
+		if(isEmpty()){
+			throw new QueueUnderflowException();
+		}
+		LLNode<T> tempNode = rear.getLink();
+		if(tempNode == rear){
+			rear = null;
+			return tempNode.getInfo();
+		}
+		rear.setLink(rear.getLink().getLink());
+		tempNode.setLink(null);
+		return tempNode.getInfo();
+	}
+
+	public void enqueue(T element) {
+		if(isEmpty()){
+			rear = new LLNode<T>(element);
+			rear.setLink(rear);
+		}else{
+			LLNode<T> tempNode = new LLNode<T>(element);
+			rear.setLink();
+			rear = rear.getLink();
+		}
+	}
+
+	public boolean isEmpty() {
+		return rear == null;
+	}
+}
