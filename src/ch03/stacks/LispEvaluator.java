aefcbc89aef9e38f5a7c9953a81c0a16fe50b7d5
leinad2018 <leinad2018@gmail.com>
2016-09-30 11:52:32 -0500

    sljdfa

--- src/ch03/stacks/LispEvaluator.java
+++ src/ch03/stacks/LispEvaluator.java
@@ -8,6 +8,7 @@ import org.omg.CORBA.DynAnyPackage.InvalidValue;
 public class LispEvaluator {
 	private LinkedStack<String> numbers;
 	private LinkedStack<Character> operators;
+
 	/**
 	 * Makes a new LispEvaluator class
 	 */
@@ -15,63 +16,93 @@ public class LispEvaluator {
 		numbers = new LinkedStack<String>();
 		operators = new LinkedStack<Character>();
 	}
-	
+
 	/**
 	 * Evaluates a valid Lisp expression<br>
 	 * Preconditions: equation is a valid expression<br>
 	 * Postconditions: returns the result of the evaluation
-	 * @param equation The expression to evaluate
+	 * 
+	 * @param equation
+	 *            The expression to evaluate
 	 * @return The value of the equation
 	 * @throws InvalidParameterException
 	 */
 	public double evaluate(String equation) throws InvalidParameterException {
-		String stuff;
-		stuff = addSpace(equation);
+		String toSplit;
+		toSplit = addSpace(equation);
 		String buffer = "";
 		String operations = "+-*/";
 
-		for (int i = 0; i < stuff.length(); i++) {
-			char temp = stuff.charAt(i);
+		String[] storage = toSplit.split(" ");
+		for (String x : storage) {
+			if (x.length() > 0) {
+				if (x.length() > 1) {
+					numbers.push(x);
 
-			if (temp == ' ') {
-				if (buffer.length() > 0) {
-					numbers.push(buffer);
-					buffer = "";
-				}
-			} else if (temp == ')') {
-				ArrayList<String> toEv = new ArrayList<String>();
+				} else if (operations.contains(x)) {
+					operators.push(x.charAt(0));
 
-				while (!(numbers.top().equals("("))) {
-					toEv.add(numbers.top());
+				} else if (x.equals(")")) {
+					ArrayList<String> toEv = new ArrayList<String>();
+
+					while (!(numbers.top().equals("("))) {
+						toEv.add(numbers.top());
+						numbers.pop();
+					}
 					numbers.pop();
+
+					char op = operators.top();
+					operators.pop();
+					numbers.push(doesMath(op, toEv));
+				} else {
+					numbers.push(x);
 				}
-				numbers.pop();
-
-				char op = operators.top();
-				operators.pop();
-				numbers.push(doesMath(op, toEv));
-
-			} else if (operations.contains(temp + "")) {
-				operators.push(temp);
-
-			} else {
-				buffer += temp;
-
 			}
 		}
+
+		// for (int i = 0; i < stuff.length(); i++) {
+		// char temp = stuff.charAt(i);
+		//
+		// if (temp == ' ') {
+		// if (buffer.length() > 0) {
+		// System.out.println(buffer);
+		// numbers.push(buffer);
+		// buffer = "";
+		// }
+		// } else if (temp == ')') {
+		// ArrayList<String> toEv = new ArrayList<String>();
+		//
+		// while (!(numbers.top().equals("("))) {
+		// toEv.add(numbers.top());
+		// numbers.pop();
+		// }
+		// numbers.pop();
+		//
+		// char op = operators.top();
+		// operators.pop();
+		// numbers.push(doesMath(op, toEv));
+		//
+		// } else if (operations.contains(temp + "")) {
+		// operators.push(temp);
+		//
+		// } else {
+		// buffer += temp;
+		//
+		// }
+		// }
 		double result = Double.parseDouble(numbers.top());
 		numbers.pop();
 		return result;
 	}
-	
-	//Adds spaces to parenthesis so that the algorithm will work
+
+	// Adds spaces to parenthesis so that the algorithm will work
 	private String addSpace(String equation) {
 		String toReturn = equation.replaceAll("\\(", "( ");
 		toReturn = toReturn.replaceAll("\\)", " )");
 		return toReturn;
 	}
 
-	//Does all of the math for the evaluate method
+	// Does all of the math for the evaluate method
 	private String doesMath(char operation, ArrayList<String> numbers)
 			throws IllegalArgumentException {
 		double toReturn = 0;
@@ -88,12 +119,13 @@ public class LispEvaluator {
 			if (numbers.size() == 0) {
 				throw new IllegalArgumentException(
 						"Subtraction needs at least 1 operand");
-			}if(numbers.size() == 1){
+			}
+			if (numbers.size() == 1) {
 				toReturn = -Double.parseDouble(numbers.get(0));
 				break;
 			}
-			toReturn = Double.parseDouble(numbers.get(numbers.size()-1));
-			for (int i = numbers.size()-2; i >= 0; i--) {
+			toReturn = Double.parseDouble(numbers.get(numbers.size() - 1));
+			for (int i = numbers.size() - 2; i >= 0; i--) {
 				toReturn -= Double.parseDouble(numbers.get(i));
 			}
 			break;
@@ -103,16 +135,16 @@ public class LispEvaluator {
 				throw new IllegalArgumentException(
 						"Division needs at least 1 operand");
 			}
-			if(numbers.size() == 1){
+			if (numbers.size() == 1) {
 				double temp = Double.parseDouble(numbers.get(0));
-				if(temp == 0){
+				if (temp == 0) {
 					throw new RuntimeException("Divide by Zero");
 				}
-				return "" + 1/temp;
+				return "" + 1 / temp;
 			}
-			toReturn = Double.parseDouble(numbers.get(numbers.size()-1));
-			for (int i = numbers.size()-2; i >= 0; i--) {
-				if(Double.parseDouble(numbers.get(i)) == 0){
+			toReturn = Double.parseDouble(numbers.get(numbers.size() - 1));
+			for (int i = numbers.size() - 2; i >= 0; i--) {
+				if (Double.parseDouble(numbers.get(i)) == 0) {
 					throw new RuntimeException("Divide by Zero");
 				}
 				toReturn /= Double.parseDouble(numbers.get(i));
--- src/ch03/stacks/LispTest.java
+++ src/ch03/stacks/LispTest.java
@@ -11,5 +11,7 @@ public class LispTest {
 		}
 		System.out.println("Test Case 2: " + ev.evaluate("(- (* (- 11 3 0) (+ 9 7 (/ (*) 2) 10)))"));
 		System.out.println("Test Case 3: " + ev.evaluate("(/ (+ 24 (- (- 12) 12)) (*) (+ (+) 1))"));
+		System.out.println("Test Case 4: " + ev.evaluate("(* (+ (*) (*) (*)) (+ (* (+ (*) (*) (*) (*)) (+ (*) (*) (*) (*) (*))) (*) (*) (*)))"));
+		System.out.println("Test Case 5: " + ev.evaluate("(* 5.2 3 (/ 2 3 2.5) 7 (- 5.3 1.2 -3.3) (/ (+ 2.3 1.0 (+) (*) (- 5.3))))"));
 	}
 }
