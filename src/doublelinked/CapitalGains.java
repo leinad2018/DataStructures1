0f44c9242a26677a522fecc8001bfb0cc56df649
leinad2018 <leinad2018@gmail.com>
2016-10-07 11:48:45 -0500

    Started capital gains

--- src/doublelinked/CapitalGains.java
+++ src/doublelinked/CapitalGains.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.Scanner;
 
 public class CapitalGains {
-	private DList<String> transactions;
+	private DList<String[]> transactions;
 	private String companies;
 
 	public CapitalGains(String stocksFilePath, String transFilePath) throws Exception {
@@ -18,12 +18,21 @@ public class CapitalGains {
 		}
 		input = new Scanner(new File(transaction));
 		while (input.hasNextLine()) {
-			String next = input.nextLine();
-			if (companies.contains(next)) transactions.addToFront(next);
+			String[] next = input.nextLine().split(";");
+			next[3] = next[3].substring(1);
+			if (companies.contains(next[0])) transactions.addToFront(next);
 			else {
 				input.close();
 				throw new Exception("Invalid Transaction Added");
 			}
 		}
+	}
+	
+	public double getCapitalGain(String company) throws Exception{
+		if(!companies.contains(company)){
+			throw new Exception("Company not valid");
+		}
+		
+		return 0.0;
 	}
 }
